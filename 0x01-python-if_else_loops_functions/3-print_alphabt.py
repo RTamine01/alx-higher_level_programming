faead5a044a2445b3342b5272990c66714fe21ae
RTamine01 <amne0002@gmail.com>
2023-11-28 16:02:19 -0800

    if else loops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
+'''#!/usr/bin/python3
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) is not 'e':
-        print("{}".format(chr(letter)))
+        print("{}".format(chr(letter)))'''
+#!/usr/bin/python3
+for c in range(ord('a'), ord('z') + 1):
+    if c != ord('e') and c != ord('q'):
+        print("{:c}".format(c), end="")
