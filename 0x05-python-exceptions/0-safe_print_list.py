ec0069286cf01cf33a3426a7d5b39a51fe890c15
RTamine01 <amne0002@gmail.com>
2023-12-18 16:15:52 -0800

    except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    
-    gh = 0
+
+    ret = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            gh += 1
+            ret += 1
         except IndexError:
             break
     print("")
-    return (gh)
+    return (ret)
