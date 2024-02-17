d8d7e2d89bb2133b6ffb2a69d6084f061219a20c
RTamine01 <amne0002@gmail.com>
2024-02-17 17:17:25 +0000

    task7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,10 @@
 -- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+INSERT
+INTO first_table (
+    id,
+    name
+)
+VALUES (
+    89,
+    'Holberton School'
+)
