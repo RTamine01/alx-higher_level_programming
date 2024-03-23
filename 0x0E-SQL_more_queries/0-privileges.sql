f69e56d4102f808b6c8f3f1359dfe8e9a12bf59c
RTamine01 <amne0002@gmail.com>
2024-03-23 15:06:31 +0000

    sql more queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all privileges of users.
-SHOW GRANT FOR 'user_0d_1'@'localhost';
-SHOW GRANT FOR 'user_0d_2'@'localhost';
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
