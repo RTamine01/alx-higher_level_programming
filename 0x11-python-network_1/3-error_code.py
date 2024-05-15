80a408cff5a8ac87d74ca501976c7187a3f5fe9a
RTamine01 <amne0002@gmail.com>
2024-05-15 22:39:28 +0000

    mandatory tasks

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" takes in a URL, and displays the value of the X-Request-Id """
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as r:
+            print(r.read().decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
